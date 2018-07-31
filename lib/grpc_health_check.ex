67849ea53f23960a156df6e6d800751a30c4c199
Milica Nerlović <milica.nerlovic@gmail.com>
2018-07-31 09:54:57 +0200

    Don't run as a separate app

--- lib/grpc_health_check.ex
+++ lib/grpc_health_check.ex
@@ -1,17 +1,2 @@
 defmodule GrpcHealthCheck do
-  use Application
-  require Logger
-
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    Logger.info("Running application in #{Mix.env()} environment")
-
-    children = [
-      supervisor(GRPC.Server.Supervisor, [{GrpcHealthCheck.Server, 50055}])
-    ]
-
-    opts = [strategy: :one_for_one, name: __MODULE__]
-    Supervisor.start_link(children, opts)
-  end
 end
