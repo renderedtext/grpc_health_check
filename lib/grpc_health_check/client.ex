9d73df0ca17b236527170600e10e59969bcd8876
Milica Nerlović <milica.nerlovic@gmail.com>
2018-07-31 09:35:53 +0200

    Enusre gun is started

--- lib/grpc_health_check/client.ex
+++ lib/grpc_health_check/client.ex
@@ -1,8 +1,11 @@
 defmodule GrpcHealthCheck.Client do
 
   def call do
+    Application.ensure_all_started(:gun)
+
     {:ok, channel} = GRPC.Stub.connect("localhost:50051")
     request = Grpc.Health.V1.HealthCheckRequest.new(service: "healthcheck")
+
     {:ok, reply} = channel |> Grpc.Health.V1.Health.Stub.check(request)
   end
 end
