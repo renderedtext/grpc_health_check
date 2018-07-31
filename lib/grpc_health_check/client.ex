79bf6bfcec1a37adf6652f97a8d54c8516173985
Milica Nerlović <milica.nerlovic@gmail.com>
2018-07-31 09:55:48 +0200

    Use the standard port

--- lib/grpc_health_check/client.ex
+++ lib/grpc_health_check/client.ex
@@ -3,7 +3,7 @@ defmodule GrpcHealthCheck.Client do
   def call do
     Application.ensure_all_started(:gun)
 
-    {:ok, channel} = GRPC.Stub.connect("localhost:50055")
+    {:ok, channel} = GRPC.Stub.connect("localhost:50051")
     request = Grpc.Health.V1.HealthCheckRequest.new(service: "healthcheck")
 
     {:ok, reply} = channel |> Grpc.Health.V1.Health.Stub.check(request)
