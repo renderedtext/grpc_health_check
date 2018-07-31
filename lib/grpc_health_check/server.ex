e1c0ef192d1f77399cc7e9539ba1da91b982b8c0
Milica Nerlović <milica.nerlovic@gmail.com>
2018-07-31 11:53:24 +0200

    Add more logging on server end

--- lib/grpc_health_check/server.ex
+++ lib/grpc_health_check/server.ex
@@ -1,9 +1,12 @@
 defmodule GrpcHealthCheck.Server do
   use GRPC.Server, service: Grpc.Health.V1.Health.Service
+  require Logger
 
   alias Grpc.Health.V1.HealthCheckResponse
 
   def check(_req, _stream) do
+    Logger.info "Received healthcheck request"
+
     HealthCheckResponse.new(
       status: HealthCheckResponse.ServingStatus.value(:SERVING)
     )
