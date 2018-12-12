c7f78cd72a5eb4d7e5577eef42705963bcd1c5ef
Milica Nerlovic <milica.nerlovic@gmail.com>
2018-12-12 14:44:10 +0100

    Remove unused code

--- lib/grpc_health_check.ex
+++ lib/grpc_health_check.ex
@@ -1,13 +1,2 @@
 defmodule GrpcHealthCheck do
-  require Logger
-  use Application
-
-  def start(_type, _args) do
-    Logger.info "GRPC Healthcheck started"
-
-    children = []
-
-    opts = [strategy: :one_for_one, name: GrpcHealthCheck]
-    Supervisor.start_link(children, opts)
-  end
 end
--- test/grpc_health_check_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GrpcHealthCheckTest do
-  use ExUnit.Case
-  doctest GrpcHealthCheck
-
-  test "greets the world" do
-    assert GrpcHealthCheck.hello() == :world
-  end
-end
