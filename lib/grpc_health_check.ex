518f36772abb189b8a0b370a5071f2eb181151f1
Radosław Woźniak <rad.wozniak@gmail.com>
2019-08-01 22:17:38 +0200

    adds Task.Supervisor

--- lib/grpc_health_check.ex
+++ lib/grpc_health_check.ex
@@ -3,11 +3,15 @@ defmodule GrpcHealthCheck do
   use Application
 
   def start(_type, _args) do
+    import Supervisor.Spec
     Logger.info "GRPC Healthcheck started"
 
-    children = []
+    children = [
+      supervisor(Task.Supervisor, [[name: GrpcHealthCheck.TaskSupervisor]])
+    ]
 
-    opts = [strategy: :one_for_one, name: GrpcHealthCheck]
+    opts = [strategy: :one_for_one, name: GrpcHealthCheck.Supervisor]
+
     Supervisor.start_link(children, opts)
   end
 end
--- lib/grpc_health_check/client.ex
+++ lib/grpc_health_check/client.ex
@@ -4,11 +4,16 @@ defmodule GrpcHealthCheck.Client do
   def call do
     Application.ensure_all_started(:gun)
 
+    healthcheck = Task.Supervisor.async_nolink(GrpcHealthCheck.TaskSupervisor, fn -> request end)
+    {:ok, check} = Task.yield(healthcheck)
+
+    Logger.info "Healthcheck request completed"
+  end
+
+  def request do
     {:ok, channel} = GRPC.Stub.connect("localhost:50051")
     request = Grpc.Health.V1.HealthCheckRequest.new(service: "healthcheck")
 
     {:ok, reply} = channel |> Grpc.Health.V1.Health.Stub.check(request)
-
-    Logger.info "Healthcheck request completed"
   end
 end
